c530757848dd66770c02b9467face5d05fa82dc5
Bianca Chery <127991428+BiancaChery@users.noreply.github.com>
2023-05-18 01:45:13 +0000

    Unfortunately lost part of my code, I have this now

--- js/sales.js
+++ js/sales.js
@@ -10,14 +10,14 @@ let stores = [
 
 let hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
 
-for(let i = 0; i < hours.length; i++){
-    stores[i].salesPerHour = []
-    let randomInt = getRandomInt(stores[i].minCust, stores[i].maxCust)
-    let saleThisHour = randomInt * stores[i].avgCookiesPerSale
-    stores[i].salesPerHour.push(saleThisHour.toFixed(0));
-}
+// for(let i = 0; i < hours.length; i++){
+//     stores[i].salesPerHour = []
+//     let randomInt = getRandomInt(stores[i].minCust, stores[i].maxCust)
+//     let saleThisHour = randomInt * stores[i].avgCookiesPerSale
+//     stores[i].salesPerHour.push(saleThisHour.toFixed(0));
+// }
 
-console.log(stores);
+// console.log(stores);
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -25,8 +25,11 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+let parentElem = document.getElementById("sales");
+
 for(let i = 0; i < stores.length; i++){
     let headingElem = document.createElement('h2');
-    headingElem.textContent = stores[i].store
+    headingElem.textContent = stores[i].store;
     parentElem.appendChild(headingElem)
 }
+
