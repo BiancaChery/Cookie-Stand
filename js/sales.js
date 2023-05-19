46dd57ef2f76f4ab6e0e0df21f70b01fb08f55aa
Bianca Chery <127991428+BiancaChery@users.noreply.github.com>
2023-05-19 00:34:52 +0000

    Added extra coding, I'm struggling with this though

--- js/sales.js
+++ js/sales.js
@@ -1,5 +1,25 @@
 "use strict";
 
+let seattleObject = {storeName: "Seattle", minCust: 23, maxCust: 65}
+cust12 = rand(23,65);
+sales12 = 6.3 * cust12
+
+let tokyoObject = {storeName: "Tokyo", minCust: 3, maxCust: 24}
+cust12 = rand(3,24);
+sales12 = 1.2 * cust12 
+
+let dubaiObject = {storeName: "Dubai", minCust: 11, maxCust: 38}
+cust12 = rand(11,38);
+sales12 = 3.7 * cust12
+
+let parisObject = {storeName: "Paris", minCust: 20, maxCust: 38}
+cust12 = rand(20,38);
+sales12 = 2.3 * cust12
+
+let limaObject = {storeName: "Lima", minCust: 2, maxCust: 16}
+cust12 = rand(2,16);
+sales12 = 4.6 * cust12 
+
 let stores = [
     { store: "Seattle", minCust: 23, maxCust: 65, avgCookiesPerSale: 6.3, },
     { store: "Tokyo", minCust: 3, maxCust: 24, avgCookiesPerSale: 1.2 },
@@ -19,6 +39,8 @@ for(let i = 0; i < hours.length; i++){
 
 console.log(stores);
 
+//https://developer.mozilla.org/en-US
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -30,3 +52,4 @@ for(let i = 0; i < stores.length; i++){
     headingElem.textContent = stores[i].store
     parentElem.appendChild(headingElem)
 }
+
