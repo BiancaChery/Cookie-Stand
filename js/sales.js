0a0fbfec147aa5d52cd90c2aa48052de1848616d
Bianca Chery <127991428+BiancaChery@users.noreply.github.com>
2023-05-20 20:54:21 +0000

    Some more added code, I need more help

--- js/sales.js
+++ js/sales.js
@@ -20,18 +20,6 @@
 // cust12 = rand(2,16);
 // sales12 = 4.6 * cust12 
 
-function Store(location, hours, totals) {
-    this.location = location;
-    this.hour = hours;
-    this.total = totals;
-}
-
-Store.prototype.storeReport = function() {
-    console.log(`The totals for these stores are ${this.location} at ${this.hour} is ${this.total}.`);
-}
-
-
-
 let stores = [
     { store: "Seattle", minCust: 23, maxCust: 65, avgCookiesPerSale: 6.3, },
     { store: "Tokyo", minCust: 3, maxCust: 24, avgCookiesPerSale: 1.2 },
@@ -40,7 +28,50 @@ let stores = [
     { store: "Lima", minCust: 2, maxCust: 16, avgCookiesPerSale: 4.6 },
 ];
 
-let hours = ["11am", "12pm", "1pm", "2pm"];
+let hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm","7pm"];
+
+function Store(location, hours, totals) {
+    this.location = location;
+    this.hour = hours;
+    this.total = totals;
+}
+
+let state = {
+    tableElem: document.getElementById("cookie-table")
+}
+state.tableElem.appendChild(...);
+
+let store1 = new Store("Seattle", "hours", )
+let store2 = new Store("Tokyo", "hours", )
+let store3 = new Store("Dubai", "hours", )
+let store4 = new Store("Paris", "hours", )
+let store5 = new Store("Lima", "hours", ) 
+console.log(store1, store2, store3, store4, store5);
+
+state.stores.push(store1)
+state.stores.push(store2)
+state.stores.push(store3)
+state.stores.push(store4)
+state.stores.push(store5)
+
+console.log(state.stores);
+
+for (let store of state.stores) {
+    store.render();
+}
+
+// store1.render();
+// store2.render();
+// store3.render();
+// store4.render();
+// store5.render();
+
+Store.prototype.render = function() {
+    let storeRow = document.createElement("tr");
+    let storeLocationTD = document.createElement("td");
+    storeLocationTD.textContent = this.location; 
+    state.tableElem.appendChild(storeRow);
+}
 
 //https://developer.mozilla.org/en-US
 
@@ -50,7 +81,6 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-
 // for(let i = 0; i < hours.length; i++){
 //     stores[i].salesPerHour = []
 //     let randomInt = getRandomInt(stores[i].minCust, stores[i].maxCust)
@@ -59,12 +89,6 @@ function getRandomInt(min, max) {
 // }
 
 // console.log(stores);
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
 
 let parentElem = document.getElementById("sales");
 
